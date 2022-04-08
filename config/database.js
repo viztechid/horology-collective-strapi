53a862403a1d55f213b84c714e34fcef691fffb4
nooglersoon <nooglersoon@outlook.co.id>
2022-04-08 13:33:22 +0700

    Configure new database using heroku

--- config/database.js
+++ config/database.js
@@ -2,12 +2,14 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'horology-collective-strapi'),
-      user: env('DATABASE_USERNAME', 'fauziabd'),
-      password: env('DATABASE_PASSWORD', 'StrapiAdmin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', 'd9fgrkc867elpv'),
+      user: env('DATABASE_USERNAME', 'wkvewduriuvors'),
+      password: env('DATABASE_PASSWORD', '36e5fe4cf138fe0963bb1d0527a49a047d87102d274139a4ca69e1c916a0392c'),
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
   },
 });
